ad2642d405be380e943cd7c3a0a8f72f6b2827b2
Nick Smedira <nsmedira@me.com>
2020-08-28 09:27:05 -0400

    add google fonts preloader to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+
+    // 08.28.20 - ADD GOOGLE FONTS PREFETCH
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Playfair Display`
+          }
+        ]
+      }
+    }
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
