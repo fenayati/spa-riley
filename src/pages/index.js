df4b3b28fea2819ccb28a0c38513ee2f870bf478
Nick Smedira <nsmedira@me.com>
2020-08-28 09:27:48 -0400

    build out home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,33 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+// STYLES
+import '../styles/index.css'
+
+// IMAGES
+import rocks from '../images/stones-167089.jpg'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <h1 className="has-text-white" style={{fontSize: "72px"}}>Spa Riley: Putting Self Love First</h1>
+        </div>
+        
+        {/* <div className="column" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}> */}
+        <div className="column">
+          {/* <Image /> */}
+          <img src={rocks} />
+        </div>
+      </div>
     </div>
+
     {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
     {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+
   </Layout>
 )
 
