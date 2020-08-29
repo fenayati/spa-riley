874b163ce49157b5fe9c6a2e0b1b0b977c2d86a5
Nick Smedira <nsmedira@me.com>
2020-08-29 18:52:52 -0400

    update main div top margin and make footer text white

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,13 +31,13 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
-          margin: `0 auto`,
+          margin: `3em auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer>
+        <footer className="has-text-white">
           © {new Date().getFullYear()}, Spa Riley, LLC
           {/* {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a> */}
