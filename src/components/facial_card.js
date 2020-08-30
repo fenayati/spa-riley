c76cc88f476141a098be1b04724f3e5130aa7102
Nick Smedira <nsmedira@me.com>
2020-08-30 19:39:32 -0400

    create a component for the facial card

---
+++ src/components/facial_card.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Img from 'gatsby-image'
+
+const FacialCard = ( {image, title, subtitle, description, columnSize} ) => {
+
+    return(
+        <div className={`column${columnSize ? " " + columnSize : ""}`}>
+            <div className="card">
+            <div className="card-image">
+                <figure className="image is-square pt-0">
+                <Img fluid={image} />
+                </figure>
+            </div>
+            <div className="card-content has-background-black">
+                <div className="content">
+                <h2 className="brown has-text-centered" style={{textTransform: "uppercase"}}>{title}</h2>
+                <h3 className="has-text-white has-text-centered">{subtitle}</h3>
+                <p className="has-text-white has-text-centered">
+                    {description}
+                </p>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default FacialCard
