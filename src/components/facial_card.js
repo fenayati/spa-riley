c0ff3768f458e00ab33c2ecaf8d5424e9471c0ff
Nick Smedira <nsmedira@me.com>
2020-08-31 09:36:57 -0400

    fix indendation

--- src/components/facial_card.js
+++ src/components/facial_card.js
@@ -6,20 +6,20 @@ const FacialCard = ( {image, title, subtitle, description, columnSize} ) => {
     return(
         <div className={`column${columnSize ? " " + columnSize : ""}`}>
             <div className="card">
-            <div className="card-image">
-                <figure className="image is-square pt-0">
-                <Img fluid={image} />
-                </figure>
-            </div>
-            <div className="card-content has-background-black">
-                <div className="content">
-                <h2 className="brown has-text-centered" style={{textTransform: "uppercase"}}>{title}</h2>
-                <h3 className="has-text-white has-text-centered">{subtitle}</h3>
-                <p className="has-text-white has-text-centered">
-                    {description}
-                </p>
+                <div className="card-image">
+                    <figure className="image is-square pt-0">
+                    <Img fluid={image} />
+                    </figure>
                 </div>
-            </div>
+                <div className="card-content has-background-black">
+                    <div className="content">
+                    <h2 className="brown has-text-centered" style={{textTransform: "uppercase"}}>{title}</h2>
+                    <h3 className="has-text-white has-text-centered">{subtitle}</h3>
+                    <p className="has-text-white has-text-centered">
+                        {description}
+                    </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
