211dadb9d741979751cc3cbcb197cb19aa91ec6e
Nick Smedira <nsmedira@me.com>
2020-08-31 09:38:01 -0400

    force insta embed on every page load

--- src/pages/products.js
+++ src/pages/products.js
@@ -7,17 +7,32 @@ import SEO from "../components/seo"
 
 const Products = () => {
 
-  const embed = function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/in.js"; d.getElementsByTagName("head")[0].appendChild(js);}
+  const embed = function(d, s, id){
+    var js
+    var scriptTag = d.getElementById(id)
+    if (scriptTag) {
+
+      // 08.31.20 WHEN NAVIGATING AWAY FROM THE PAGE AND BACK AGAIN, THE SCRIPT ALREADY EXISTS IN THE HEAD, AND SO THE INSTAGRAM FEED IS NOT REEMBEDDED WHEN NAVIGATING BACK
+      // console.log('element w/ id: ', id, 'already exists')
+      // return
+      scriptTag.parentNode.removeChild(scriptTag)
+
+    } 
+    js = d.createElement(s)
+    js.id = id
+    js.src = "https://embedsocial.com/embedscript/in.js"
+    d.getElementsByTagName("head")[0].appendChild(js)
+  }
 
   useEffect(() => {
     embed(document, "script", "EmbedSocialInstagramScript");
-  })
+  }, [])
 
   return (    
     <Layout>
       <SEO title="Products" />
 
-      <h1>Product page</h1>
+      <h1 className="has-text-white">Product page</h1>
       <div className="container">
 
         {/* 08.30.20: EMBED INSTAGRAM FEED */}
