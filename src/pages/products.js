97c70448646afb6ec1b3e19faa9419d8a8f8a6b6
Nick Smedira <nsmedira@me.com>
2020-09-01 21:42:26 -0400

    moved embedsocial data_ref into .env file

--- src/pages/products.js
+++ src/pages/products.js
@@ -33,7 +33,7 @@ const Products = () => {
       <div className="container">
 
         {/* 08.30.20: EMBED INSTAGRAM FEED */}
-        <div class='embedsocial-instagram' data-ref="71f47bcc8d931b7b1347e60a9528c2d3927f6464" style={{}}></div>
+        <div class='embedsocial-instagram' data-ref={process.env.EMBED_SOCIAL_DATA_REF} style={{}}></div>
         {/* PUT THE SCRIPT INSIDE USEFFECT HOOK */}
         {/* <script>(function(d, s, id){var js; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://embedsocial.com/embedscript/in.js"; d.getElementsByTagName("head")[0].appendChild(js);}(document, "script", "EmbedSocialInstagramScript"));</script> */}
 
