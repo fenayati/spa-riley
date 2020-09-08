8f58e72a3e11a82660cafb1456a08a47a5a91ee4
Nick Smedira <nsmedira@me.com>
2020-09-08 11:38:35 -0400

    update look of facial cards

--- src/components/facial_card.js
+++ src/components/facial_card.js
@@ -11,13 +11,13 @@ const FacialCard = ( {image, title, subtitle, description, columnSize} ) => {
                     <Img fluid={image} />
                     </figure>
                 </div>
-                <div className="card-content has-background-black">
-                    <div className="content">
-                    <h2 className="brown has-text-centered" style={{textTransform: "uppercase"}}>{title}</h2>
-                    <h3 className="has-text-white has-text-centered">{subtitle}</h3>
-                    <p className="has-text-white has-text-centered">
-                        {description}
-                    </p>
+                <div className="card-content" style={{backgroundColor: "#2F3E46"}}>
+                    <div className="content" style={{color: "#CAD2C5"}}>
+                        <h2 className="has-text-centered" style={{textTransform: "uppercase", color: "#CAD2C5"}}>{title}</h2>
+                        <h3 className="has-text-centered" style={{color: "#CAD2C5"}}>{subtitle}</h3>
+                        <p className="has-text-centered">
+                            {description}
+                        </p>
                     </div>
                 </div>
             </div>
