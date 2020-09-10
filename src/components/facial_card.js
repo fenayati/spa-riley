17e4802b64d6224833f72e191cd2612ee758f75d
Nick Smedira <nsmedira@me.com>
2020-09-10 09:25:13 -0400

    make all facial cards appear the same height and shorten the images

--- src/components/facial_card.js
+++ src/components/facial_card.js
@@ -5,13 +5,13 @@ const FacialCard = ( {image, title, subtitle, description, columnSize} ) => {
 
     return(
         <div className={`column${columnSize ? " " + columnSize : ""}`}>
-            <div className="card">
+            <div className="card" style={{height: "100%", backgroundColor: "#2F3E46"}}>
                 <div className="card-image">
-                    <figure className="image is-square pt-0">
-                    <Img fluid={image} />
+                    <figure className="image">
+                    <Img fluid={image} style={{width: "100%", height: 0, paddingTop: "75%"}} />
                     </figure>
                 </div>
-                <div className="card-content" style={{backgroundColor: "#2F3E46"}}>
+                <div className="card-content">
                     <div className="content" style={{color: "#CAD2C5"}}>
                         <h2 className="has-text-centered" style={{textTransform: "uppercase", color: "#CAD2C5"}}>{title}</h2>
                         <h3 className="has-text-centered" style={{color: "#CAD2C5"}}>{subtitle}</h3>
