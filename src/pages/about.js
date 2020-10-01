d24648344524a7ea5b9586aa2022869b82bea917
nsmedira <nsmedira@me.com>
2020-10-01 11:08:08 -0400

    update about me text

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,15 @@ const About = ({data}) => (
       <div className="column has-text-centered has-text-white" style={{backgroundColor: "#2f3e46", padding: 0}}>
         <div style={{padding: "2em"}}>
           <h1>Riley White</h1>
-          <p>My name is Riley and I am the proud owner of Spa Riley. I grew up in Chagrin Falls and have spent time in other places but knew this would be the perfect setting for my business. Growing up I experienced acne as a teen and often felt like going to an esthician was embarassing. My goal is to provide a safe sanctuary where clients feel comfortable and at my space and trust me to help with thier skin journey. I believe our skins health is impacted by outside and inside factors so I like to get to know my clients and come up with a homecare routine that works. Whether you are struggling with acne, loss of elasticity, hyperpigmentation or any other skin concerns, I am here to help! In my off time I am usually reading about new skincare or makeup breakthroughs and am a firm believer in cruelty free, vegan and organic products. I look at taking the time to come to the spa as carving out a little time for yourself, which is very important. Book your first service and lets get started to making you feel as fabulous as you really are!</p>
+
+          <p>My name is Riley and I am the proud owner of Spa Riley. I grew up in Chagrin Falls and have spent time in other places but felt being here would be the right setting for my business. Growing up I experienced acne as a teen and often felt like going to an esthetician was embarassing.</p>
+
+          <p>My goal is to provide a safe sanctuary where clients feel comfortable and are able to trust me with their skin journey. I believe our skins health is both impacted by outside and inside factors, so I like to get to know my clients and come up with a home care routine that works with their lifestyle. Whether you are experiencing acne, loss of elasticity, hyperpigmentation or other skin concerns, I am here to help!</p>
+
+          <p>Outside of the spa, I spend more time than I'd like to admit, talking to my two dogs. I look at taking the time to come to the spa as carving out a little time for yourself, which is very important.</p>
+
+          <p>Book your first service and lets get started to making you feel as fabulous as you really are!</p>
+          
         </div>
       </div>
     </div>
