8c7572df67ba86af3219243955599d83f932f435
Nick Smedira <nsmedira@me.com>
2020-10-12 19:42:45 -0400

    create bulma components

---
+++ src/components/layout/bulma/columns.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Columns = ( {children} ) => {
+    return (
+        <div className="columns">
+            {children}
+        </div>
+    )
+}
+
+export default Columns
---
+++ src/components/layout/bulma/index.js
@@ -0,0 +1,9 @@
+import Column from "./column"
+import Columns from "./columns"
+import Container from "./container"
+
+export {
+    Column,
+    Columns,
+    Container
+}
