53722347109bade09ee97b8ce24be8e42586e8b7
Nick Smedira <nsmedira@me.com>
2020-10-12 19:07:58 -0400

    import layout.js from new layout folder

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Img from 'gatsby-image'
 
-import Layout from "../components/layout"
+import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
 const About = ({data}) => (
