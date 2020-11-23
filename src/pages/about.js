d3c57c946038e9d7515755a055e07323085364df
Nick Smedira <nsmedira@me.com>
2020-11-22 19:36:30 -0500

    fix types on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -62,11 +62,11 @@ const About = ({data}) => (
       
                 <p>My name is Riley and I am the proud owner of Spa Riley. I grew up in Chagrin Falls and have spent time in other places but felt being here would be the right setting for my business. Growing up I experienced acne as a teen and often felt like going to an esthetician was embarassing.</p>
       
-                <p>My goal is to provide a safe sanctuary where clients feel comfortable and are able to trust me with their skin journey. I believe our skins health is both impacted by outside and inside factors, so I like to get to know my clients and come up with a home care routine that works with their lifestyle. Whether you are experiencing acne, loss of elasticity, hyperpigmentation or other skin concerns, I am here to help!</p>
+                <p>My goal is to provide a safe sanctuary where clients feel comfortable and are able to trust me with their skin journey. I believe our skin's health is both impacted by outside and inside factors, so I like to get to know my clients and come up with a home care routine that works with their lifestyle. Whether you are experiencing acne, loss of elasticity, hyperpigmentation or other skin concerns, I am here to help!</p>
       
-                <p>Outside of the spa, I spend more time than I'd like to admit, talking to my two dogs. I look at taking the time to come to the spa as carving out a little time for yourself, which is very important.</p>
+                <p>Outside of the spa, I spend more time than I'd like to admit talking to my two dogs. I look at taking the time to come to the spa as carving out a little time for yourself, which is very important.</p>
       
-                <p>Book your first service and lets get started to making you feel as fabulous as you really are!</p>
+                <p>Book your first service and let's get started to making you feel as fabulous as you really are!</p>
                 
               </div>
       
