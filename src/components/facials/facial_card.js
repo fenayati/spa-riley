5e0fbc2d06ffd967b70b7dc6432feebb4b78944c
Nick Smedira <nsmedira@me.com>
2020-11-25 21:09:20 -0500

    overhaul facial card

--- src/components/facials/facial_card.js
+++ src/components/facials/facial_card.js
@@ -1,36 +1,30 @@
 import React from "react"
 import Img from 'gatsby-image'
-import { Col } from 'react-bootstrap'
+import { Col, Card } from 'react-bootstrap'
 
 const FacialCard = ( {image, title, subtitle, description, columnSize} ) => {
 
     return(
 
         <Col>
-            <div className="card bg-color-0" style={{height: "100%"}}>
-                <div className="card-image">
-                    <figure className="image">
-                    <Img fluid={image} style={{width: "100%", height: 0, paddingTop: "75%"}} />
-                    </figure>
-                </div>
-                <div className="card-content">
-                    <div 
-                        className="content has-text-white"
-                    >
-                        <h2 className="has-text-centered has-text-white upper">
-                                {title}
-                            </h2>
-                        <h3 
-                            className="has-text-centered has-text-white"
-                        >
-                            {subtitle}
-                        </h3>
-                        <p className="has-text-centered">
-                            {description}
-                        </p>
+            <Card className="bg-color-0" style={{height: "100%", border: 0, borderRadius: 0}}>
+
+                {/* height 0 + paddingTop 75% is for the 4:3 aspect ratio */}
+                <Img fluid={image} style={{width: "100%", height: 0, paddingTop: "75%"}} />
+
+                <Card.Body>
+                
+                    <div className="text-white text-center">
+
+                        <h2 className="text-uppercase">
+                            {title}
+                        </h2>
+                        <h3>{subtitle}</h3>
+                        <p>{description}</p>
                     </div>
-                </div>
-            </div>
+                    
+                </Card.Body>
+            </Card>
         </Col>
 
     )
