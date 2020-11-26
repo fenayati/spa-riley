67faa2230e4be80dcc2d4dd46e4ec24c47510dae
Nick Smedira <nsmedira@me.com>
2020-11-25 19:58:14 -0500

    replace Columns, Column with Row, Col

--- src/pages/services/waxing.js
+++ src/pages/services/waxing.js
@@ -5,10 +5,9 @@ import Img from 'gatsby-image'
 import Layout from "../../components/layout/layout"
 import SEO from "../../components/seo"
 import WaxingService from '../../components/waxing_service'
-import { Columns, Column } from "../../components/layout/bulma"
 import Window from '../../components/layout/window'
 import VerticalCenterFlex from '../../components/layout/vertical-center-flex'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 // CSS
 import '../../styles/pages/services/waxing.css'
@@ -30,38 +29,46 @@ const Waxing = ( {data} ) => {
 
           <div className="super-container">
             <Container>
-              <Columns>
+              <Row>
         
-                <Column classes={["has-text-centered", "has-text-white", "bg-color-0", "pad-0"]}>
+                <Col>
         
-                  <div style={{padding: "2em"}}>
-                    <h2 className="has-text-centered has-text-white upper">
-                      Waxing
-                    </h2>
-                    <div>
-                      {
-                        waxingServices.map(service => 
-                          <WaxingService 
-                            service={service.service} 
-                            price={service.price} 
-                          />
-                        )
-                      }
+                  <div className="has-text-centered has-text-white bg-color-0 pad-0">
+                    <div style={{padding: "2em"}}>
+                      <h2 className="has-text-centered has-text-white upper">
+                        Waxing
+                      </h2>
+                      <div>
+                        {
+                          waxingServices.map(service => 
+                            <WaxingService 
+                              service={service.service} 
+                              price={service.price} 
+                            />
+                          )
+                        }
+                      </div>
                     </div>
                   </div>
         
-                </Column>
+                </Col>
         
-                <Column classes={["pad-0", "border-10px-color-0"]}>
-                  <Img 
-                    fluid={data.waxingLegs.childImageSharp.fluid}
-                    style={{
-                      height: "100%",
-                    }}
-                  />
-                </Column>
+                <Col>
+
+                  <div className="pad-0 border-10px-color-0">
+
+                    <Img 
+                      fluid={data.waxingLegs.childImageSharp.fluid}
+                      style={{
+                        height: "100%",
+                      }}
+                    />
+
+                  </div>
+                  
+                </Col>
         
-              </Columns>
+              </Row>
             </Container>
           </div>
 
