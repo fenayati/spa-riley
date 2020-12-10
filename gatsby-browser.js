38d7449e0b1a8d2143d882947c80a6dc79ed9e6e
Nick Smedira <nsmedira@me.com>
2020-12-09 21:18:27 -0500

    add base.css stylesheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 import './src/styles/sass/custom.scss'
-import './src/styles/layout/layout.css'
+import './src/styles/base.css'
+import './src/styles/layout.css'
 import './src/styles/layout/navbar.css'
 import './src/styles/layout/footer.css'
 
