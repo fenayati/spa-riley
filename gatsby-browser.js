6d127872ca9eca4e60dad5395308859cc978bd13
Nick Smedira <nsmedira@me.com>
2020-12-12 20:06:27 -0500

    import all partials into index.scss & import the one global stylesheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,14 +1,4 @@
-import './src/styles/bootstrap.scss'
-
-// MODULES
-import './src/styles/modules/primary-border.scss'
-import './src/styles/modules/navbar.scss'
-import './src/styles/modules/social-icons.scss'
-
-// BASE, LAYOUT, THEME
-import './src/styles/base.scss'
-import './src/styles/layout.scss'
-import './src/styles/theme.scss'
+import './src/styles/index.scss'
 
 // IMPORT WRAP PAGE ELEMENT
 import CustomLayout from "./wrapPageElement"
